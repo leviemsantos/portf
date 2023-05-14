ce170896a71bbf8d5fd14920ca33a846c95249f1
Levi Santos <emanuellevi0@gmail.com>
2023-05-14 10:59:03 -0300

    Stitches config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { styled } from "@stitches/react";
 import { ReactElement } from "react";
+
+const Titulo = styled("h1", {
+  color: "blue",
+})
+
+const Button = styled('button', {
+  backgroundColor: 'red',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'lightgray',
+  },
+});
+
+const Teste = () => <Button>Button</Button>
 
 export default function Inicio(): ReactElement {
   return (
     <div>
-      <h1>Começando</h1>
+      <Titulo as='h1'>Começando</Titulo>
+      <Button>Botão</Button>
+      <Teste />
     </div>
   )
 }
