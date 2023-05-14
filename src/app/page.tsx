60b3a88e709bd203c25a0824b7bc5c2892b78fd6
Levi Santos <emanuellevi0@gmail.com>
2023-05-14 11:31:46 -0300

    Radix config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,13 @@
-import { styled } from "@stitches/react";
+"use client";
+
 import { ReactElement } from "react";
-
-const Titulo = styled("h1", {
-  color: "blue",
-})
-
-const Button = styled('button', {
-  backgroundColor: 'red',
-  borderRadius: '9999px',
-  fontSize: '13px',
-  padding: '10px 15px',
-  '&:hover': {
-    backgroundColor: 'lightgray',
-  },
-});
-
-const Teste = () => <Button>Button</Button>
+import { Button } from "./pages/button"
 
 export default function Inicio(): ReactElement {
   return (
     <div>
-      <Titulo as='h1'>Começando</Titulo>
-      <Button>Botão</Button>
-      <Teste />
+      <h1>Começando</h1>
+      <Button>My Button</Button>
     </div>
   )
 }
